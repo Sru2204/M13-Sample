3d3464378cdaf0a224443b02782afbef962a9b07
SrushtiG <srushtig04@gmail.com>
2026-07-18 10:08:47 +0530

    This is FE conflict

--- src/test/java/demo/GitPractice2.java
+++ src/test/java/demo/GitPractice2.java
@@ -4,5 +4,6 @@ public class GitPractice2 {
  public static void main(String[]args) {
 	 System.out.println("Practicing Git Branch!");
 	 System.out.println("In ATE branch");
+	 System.out.println("Practice git conflict from FE system");
  }
 }
