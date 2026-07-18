811f29bee6869bb8b5b171ee37b29aff446efe6e
SrushtiG <srushtig04@gmail.com>
2026-07-18 09:19:06 +0530

    ATE branch

---
+++ src/test/java/demo/GitPractice2.java
@@ -0,0 +1,11 @@
+package demo;
+
+public class GitPractice2 {
+
+	public static void main(String[] args) {
+		System.out.println("Practicing Git Branch");
+		 System.out.println("In ATE branch");
+
+	}
+
+}
